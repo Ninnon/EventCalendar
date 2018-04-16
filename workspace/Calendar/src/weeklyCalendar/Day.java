8957cb45357216ce81743a0035e864bdd6d1ca2a
pomalone91 <pomalone91@gmail.com>
2018-04-15 21:39:17 -0600

    Added Day enum. Added Day property in Event class. Renamed Event class.

---
+++ workspace/Calendar/src/weeklyCalendar/Day.java
@@ -0,0 +1,5 @@
+package weeklyCalendar;
+
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
+}
--- workspace/Calendar/src/weeklyCalendar/Event.java
+++ workspace/Calendar/src/weeklyCalendar/Event.java
@@ -2,44 +2,38 @@ package weeklyCalendar;
 
 import java.util.Date;
 
-public class Events {
+public class Event {
+	Day eventDay;
 	String eventTitle;
 	Date startTime;
 	String description;
 	
 	
-	public Events(String eventTitle, Date startTime, String description) {
+	public Event(String eventTitle, Date startTime, String description) {
 		this.eventTitle = eventTitle;
 		this.startTime = startTime;
 		this.description = description;
 	}
 
-
+	public Day getEventDay() {
+		return eventDay; }
 	public String getEventTitle() {
 		return eventTitle;
 	}
-
-
-	public void setEventTitle(String eventTitle) {
-		this.eventTitle = eventTitle;
-	}
-
-
 	public Date getStartTime() {
 		return startTime;
 	}
-
-
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}
-
-
 	public String getDescription() {
 		return description;
 	}
 
-
+	public void setEventDay(Day eventDay) { this.eventDay = eventDay; }
+	public void setEventTitle(String eventTitle) {
+		this.eventTitle = eventTitle;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
 	public void setDescription(String description) {
 		this.description = description;
 	}
